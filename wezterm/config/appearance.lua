eec9af1f14ab3cf52b54234c92cba66e4cee6a16
zjw <zjw1858@163.com>
2026-06-09 19:38:35 +0800

    update tab bar

--- wezterm/config/appearance.lua
+++ wezterm/config/appearance.lua
@@ -141,7 +141,7 @@ function M.apply(config)
     -- Tab bar 设置
     config.enable_tab_bar = true
     config.hide_tab_bar_if_only_one_tab = false
-    config.use_fancy_tab_bar = false
+    config.use_fancy_tab_bar = true
     config.tab_max_width = 25
     config.show_tab_index_in_tab_bar = false
     config.switch_to_last_active_tab_when_closing_tab = true
